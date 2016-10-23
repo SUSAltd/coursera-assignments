9265e02e7d7ebbd8016a214ba7734e2292820936
SUSAltd <the_pgf@yahoo.com>
2016-10-22 21:16:57 -0700

    fixed counting empty items in some cases

--- module1_solution/app.js
+++ module1_solution/app.js
@@ -8,18 +8,37 @@ LunchCheckController.$inject = ['$scope'];
 function LunchCheckController($scope) {
   $scope.textbox = ''; // initialize to empty
 
+  /*
+    Displays message in the little message place
+  */
   $scope.displayMessage = function() {
-    var foods = $scope.textbox.split(/ *,[ *,*]*/); // ignores "empty" list items
-
-    if (isTooMuchFood(foods))
+    if (isTooMuchFood(getFoods()))
       $scope.message = 'Too much!';
     else
       $scope.message = 'Enjoy!';
   };
 
+  /*
+    Returns an array of foods parsed from the input box
+  */
+  function getFoods() {
+    var result = $scope.textbox.split(/ *,[ *,*]*/); // ignores "empty" list items
+
+    if (result[0] == '')
+      result = result.slice(1);
+    if (result[result.length - 1] == '')
+      result = result.slice(0, -1);
+
+    return result;
+  }
+
+  /*
+    Returns true if there is too much food, false otherwise
+  */
   function isTooMuchFood(foodItems) {
     return foodItems.length > 3;
   }
+
 }
 
 })();
